fde5b057398fc50b96687d076b7580eeac68aa1b
hideya <62914164+hidey7@users.noreply.github.com>
2024-05-04 14:55:28 +0900

    Initial Commit

---
+++ mosaicAppJustOnly/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  mosaicAppJustOnly
+//
+//  Created by 始関秀弥 on 2024/05/04.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ mosaicAppJustOnly/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  mosaicAppJustOnly
+//
+//  Created by 始関秀弥 on 2024/05/04.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
